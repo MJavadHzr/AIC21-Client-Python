6d6160af3504f17257bda0539d5fc5de6a74bddb
MJavad Hezareh <55049080+MJavadHzr@users.noreply.github.com>
2021-04-20 01:33:04 +0430

    sample-AI [has some problem]

--- AI.py
+++ AI.py
@@ -1,10 +1,11 @@
 from Model import *
 import random
-import json
 from typing import *
 
 
 class AI:
+    last_dir: int = Direction.UP.value
+
     def __init__(self):
         # Current Game State
         self.game: Game = None
@@ -14,15 +15,79 @@ class AI:
         self.direction: int = None
         self.value: int = None
 
-    """
-    Return a tuple with this form:
-        (message: str, message_value: int, message_dirction: int)
-    check example
-    """
+    def get_xy_value_of_direction(self, dir):
+        switcher = {
+            Direction.CENTER.value: (0, 0),
+            Direction.RIGHT.value: (1, 0),
+            Direction.UP.value: (0, -1),
+            Direction.LEFT.value: (-1, 0),
+            Direction.DOWN.value: (0, 1),
+        }
+        return switcher.get(dir, -1)
 
-    def turn(self) -> (str, int, int):
-        """self.message = "hello python"
-        self.value = random.randint(1,10)
-        self.direction = random.choice(list(Direction)).value"""
-        self.direction = Direction.UP.value
-        return (self.message, self.value, self.direction)
+    def is_wall_cell(self, agent: Ant, relative_pos):
+        neighbor_cell = agent.getMapRelativeCell(
+            x=relative_pos[0],
+            y=relative_pos[1]
+        )
+        return neighbor_cell.type == CellType.WALL.value
+
+    def is_resource_cell(self, agent: Ant, relative_pos):
+        neighbor_cell = agent.getMapRelativeCell(
+            x=relative_pos[0],
+            y=relative_pos[1]
+        )
+        return neighbor_cell.resource_type == ResourceType.BREAD.value or neighbor_cell.resource_type == ResourceType.GRASS.value
+
+    # simple example which keep its last direction till face a wall,
+    # then report wall in chat box and randomly continues its way
+    # also report in case of resource detection
+    def turn(self):
+        agent = self.game.ant
+        game = self.game
+
+        # first check chat box
+        for chat in game.chatBox.allChats:
+            print('message: {}, turn:{}'.format(chat.text, chat.turn))
+
+        # assume there next step is empty cell
+        relative_pos = self.get_xy_value_of_direction(AI.last_dir)
+        self.message = 'empty @ [{}, {}]'.format(
+            agent.currentX + relative_pos[0],
+            agent.currentY + relative_pos[1]
+        )
+        self.direction = AI.last_dir
+
+        # check if there is a wall
+        if self.is_wall_cell(agent, relative_pos):
+            # report we have found a wall
+            self.message = 'wall @ [{}, {}]'.format(
+                agent.currentX + relative_pos[0],
+                agent.currentY + relative_pos[1]
+            )
+
+            # chose a random direction
+            directions = [Direction.UP, Direction.RIGHT,
+                          Direction.DOWN, Direction.LEFT]
+            while self.direction == AI.last_dir:
+                random.shuffle(directions)
+                self.direction = directions[0].value
+            AI.last_dir = self.direction
+
+        # check if there is resource
+        elif self.is_resource_cell(agent, relative_pos):
+            # report we have found a resource
+            neighbore_cell = agent.getMapRelativeCell(
+                x=relative_pos[0],
+                y=relative_pos[1]
+            )
+
+            self.message = "resource @ [{}, {}], type:{}, value:{}".format(
+                neighbore_cell.x,
+                neighbore_cell.y,
+                neighbore_cell.resource_type,
+                neighbore_cell.resource_value
+            )
+
+        # return answer
+        return(self.message, 1, self.direction)
