044ab7493cc46f060dc55a366e47854d1829e7e2
MJavad Hezareh <55049080+MJavadHzr@users.noreply.github.com>
2021-04-20 22:34:48 +0430

    sample AI -[bugs resolved]

--- AI.py
+++ AI.py
@@ -2,6 +2,10 @@ from Model import *
 import random
 from typing import *
 
+# simple example which keep its last direction till face a wall,
+# then report wall in chat box and randomly continues its way
+# also report in case of resource detection
+
 
 class AI:
     last_dir: int = Direction.UP.value
@@ -37,11 +41,8 @@ class AI:
             x=relative_pos[0],
             y=relative_pos[1]
         )
-        return neighbor_cell.resource_type == ResourceType.BREAD.value or neighbor_cell.resource_type == ResourceType.GRASS.value
+        return (neighbor_cell.resource_type == ResourceType.BREAD.value) or (neighbor_cell.resource_type == ResourceType.GRASS.value)
 
-    # simple example which keep its last direction till face a wall,
-    # then report wall in chat box and randomly continues its way
-    # also report in case of resource detection
     def turn(self):
         agent = self.game.ant
         game = self.game
@@ -50,13 +51,14 @@ class AI:
         for chat in game.chatBox.allChats:
             print('message: {}, turn:{}'.format(chat.text, chat.turn))
 
-        # assume there next step is empty cell
+        # assume next step is empty cell
         relative_pos = self.get_xy_value_of_direction(AI.last_dir)
         self.message = 'empty @ [{}, {}]'.format(
             agent.currentX + relative_pos[0],
             agent.currentY + relative_pos[1]
         )
         self.direction = AI.last_dir
+        self.value = 1
 
         # check if there is a wall
         if self.is_wall_cell(agent, relative_pos):
@@ -66,13 +68,17 @@ class AI:
                 agent.currentY + relative_pos[1]
             )
 
-            # chose a random direction
+            # choose a random direction
             directions = [Direction.UP, Direction.RIGHT,
                           Direction.DOWN, Direction.LEFT]
-            while self.direction == AI.last_dir:
+
+            while self.is_wall_cell(agent, relative_pos):
                 random.shuffle(directions)
-                self.direction = directions[0].value
+                self.direction = random.choice(directions).value
+                relative_pos = self.get_xy_value_of_direction(self.direction)
+
             AI.last_dir = self.direction
+            self.value = 3
 
         # check if there is resource
         elif self.is_resource_cell(agent, relative_pos):
@@ -82,12 +88,14 @@ class AI:
                 y=relative_pos[1]
             )
 
-            self.message = "resource @ [{}, {}], type:{}, value:{}".format(
+            self.message = 'resource @ [{}, {}], type:{}, value:{}'.format(
                 neighbore_cell.x,
                 neighbore_cell.y,
                 neighbore_cell.resource_type,
                 neighbore_cell.resource_value
             )
 
+            self.value = 5
+
         # return answer
-        return(self.message, 1, self.direction)
+        return(self.message, self.value, self.direction)
